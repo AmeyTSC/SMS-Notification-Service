b14ec734728fc8b99fc7081917e631f0be6740fc
AmeyTSC <amey.gupta@thesleepcompany.in>
2024-10-28 18:05:30 +0530

    logger added

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,28 @@
-import { Injectable } from '@nestjs/common';
-
+import { Injectable, Logger } from '@nestjs/common';
+import { InjectConnection } from '@nestjs/mongoose';
+import { Connection } from 'mongoose';
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'SMS-Notification Service';
+  private readonly logger = new Logger(AppService.name);
+  constructor(@InjectConnection() private readonly connection: Connection) {}
+
+  ConnectionStatus(): void {
+    const status = this.connection.readyState;
+    switch (status) {
+      case 0:
+        this.logger.fatal('Disconnected');
+        break;
+      case 1:
+        this.logger.log('Connected');
+        break;
+      case 2:
+        this.logger.log('Connecting');
+        break;
+      case 3:
+        this.logger.warn('Disconnecting');
+        break;
+      default:
+        this.logger.error('Unknown status');
+    }
   }
 }
